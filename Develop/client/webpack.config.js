918a7af51fcb04b9de1a57788053608dc86fede7
Phanimal <jameslphan@hotmail.com>
2022-12-04 22:26:11 -0800

    webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,11 +18,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Tickle Text-Editor'
+      }),
+      // Service Worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }), 
+
+      // Manifest
+      new WebpackPwaManifest({
+        name: 'Text Tickle Text-Editor',
+        short_name: 'TTT-E',
+        background_color: 'black',
+        theme_color: 'yellow',
+        start_url: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
+        // CSS Loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Babel
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules|bower_components/,
+          use: {
+            loader: 'babel-loader',
+            presets: ['@babel/preset-env'],
+            plugins: ['./babel-plugin-myPlugin", "@babel/plugin-transform-runtime'],
+          }
+        }
         
       ],
     },
